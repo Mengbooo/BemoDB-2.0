24224144641652c8b544a51f13c77261178d70f6
Mengbooo <bo156431362@outlook.com>
2025-11-21 17:40:28 +0800

    feat: 配置Waline

--- src/site.config.ts
+++ src/site.config.ts
@@ -154,7 +154,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://waline-bolaxious-jj0x4em40-mengbooos-projects.vercel.app/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
